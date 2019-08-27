46fb590cd2f3e70a980d99b313cd0454273a7687
Luke Buckheit <lukebuckheit@gmail.com>
2019-08-27 01:56:49 -0400

    ScheduleList now contains all the logic required to write to the realtime database

--- sources/components/ScheduleList.js
+++ sources/components/ScheduleList.js
@@ -1,12 +1,51 @@
 import React, { Component } from "react";
 import { View, Text, ScrollView, Image, Dimensions, StyleSheet } from "react-native";
-import { Card, Button } from "react-native-elements";
+import { Button } from "react-native-elements";
 import axios from "axios";
 
 import UserLocation from "./ScheduleListMap";
 import DefaultLocation from "./UsersMap";
 import NearestCity from "../../trainStopInfo";
 import TrainCard from './TrainCard'
+
+//ALL THE FIREBASE STUFF -------------------------------------------------------
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/database");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDFJrmYkjMr6bymsyonik7Xr6zL9SMlxtA",
+  authDomain: "subwar-a2611.firebaseapp.com",
+  databaseURL: "https://subwar-a2611.firebaseio.com",
+  projectId: "subwar-a2611",
+  storageBucket: "subwar-a2611.appspot.com",
+  messagingSenderId: "909830506182",
+  appId: "1:909830506182:web:ac670ea82577cee6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Write the data to the DB and then kill the connection
+function writeTestData(testNumber, str) {
+  firebase.database().ref('test-collection/' + testNumber).set({
+    'test-field': str
+  })
+  .then(() => killConnection())
+}
+
+//Kill the firebase connection and log success
+function killConnection() {
+  firebase.app().delete().then(function() {
+    console.log('connection closed')
+  });
+}
+
+//ALL THE FIREBASE STUFF -------------------------------------------------------
 
 export default class ScheduleList extends Component {
   constructor(props) {
@@ -142,6 +181,9 @@ export default class ScheduleList extends Component {
           <Text style={styles.swipingInstructionsTextStyle}>
             Swipe left/right for uptown/downtown
           </Text>
+
+          {/* BUTTON FOR TESTING FIREBASE WRIRE FUNCTIONALITY */}
+          <Button onPress={() => writeTestData(25, 'TEST')} title="FIREBASE TEST" />
 
           {/* 'CLOSE CAMERA' BUTTON */}
           <Button
