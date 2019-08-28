d4271e3882ff43382f0a680b1247940ddb2293b3
Luke Buckheit <lukebuckheit@gmail.com>
2019-08-27 23:09:52 -0400

    cleaned up most stuff except for ScheduleList, which is still hideous

--- sources/components/CustomPopover.js
+++ sources/components/CustomPopover.js
@@ -3,10 +3,21 @@ import { View, Text, StyleSheet } from "react-native";
 import { Button } from "react-native-elements";
 
 export default function CustomTooltip(props) {
+  //A flag for whether this train is on the list of congested trains or not
+  const isCongested = props.congested.includes(props.trainTime[1])
+
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>{(props.congested.includes(props.trainTime[1]) && 'Users have reported this train is congested') || ('No users have reported this train is congested')}</Text>
-      <Button titleStyle={styles.buttonTitleStyle} buttonStyle={styles.buttonButtonStyle} title='Report congestion' onPress={() => props.writeTestData(props.trainTime[0], props.trainTime[1])}/>
+
+      {/* CHECKING THE ARRAY OF DELAYED TRAINS (CONSTRUCTED FROM THE DATABASE), AND DISPLAING THE APPROPRIATE TEXT*/}
+      <Text style={styles.textStyle}>
+        {isCongested && 'Users have reported this train is congested'}
+        {!isCongested && 'No users have reported this train congested'}
+      </Text>
+
+      {/* DISPLAYING A BUTTON THAT WILL PUT THIS TRAIN ONTO THE CONGESTED TRAINS SECTION OF THE DB */}
+      <Button titleStyle={styles.buttonTitleStyle} buttonStyle={styles.buttonButtonStyle} title='Report congestion' onPress={() => props.writeTestData(props.trainTime[0], props.trainTime[1])} />
+
     </View>
   )
 }
